37d354d9e52b0098b0776ead2d56ed72e962d175
Pablo Santos <p.santos@textecnologia.com.br>
2021-08-23 16:21:15 -0300

    using pytest parametrize

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,13 +1,28 @@
-import unittest
 from unittest.mock import Mock
+
+import pytest
 
 from tasks.utils.models.task import Task
 from tasks.utils.exceptions import StatusError
 
-class TestPoha(unittest.TestCase):
-    def setUp(self) -> None:
-        self.task = Task(Mock(), Mock())
-    
-    def test_error(self):
-        with self.assertRaises(StatusError):
-            self.task.add_status('seeing')
+@pytest.fixture
+def task() -> None:
+    return Task(Mock(), Mock())
+
+@pytest.mark.parametrize(
+    'is_valid_status, status', 
+    (
+        (True, 'done'),
+        (True, 'Done'),
+        (True, 'DONE'),
+        (True, 'failed'),
+        (False, 'seeing'),
+    )
+)
+def test_task_add_status(is_valid_status: bool, status: str, task: Task) -> None:
+    if not is_valid_status:
+        with pytest.raises(StatusError):
+            task.add_status(status)
+    else:
+        task.add_status(status)
+        assert task.status == status.lower()
